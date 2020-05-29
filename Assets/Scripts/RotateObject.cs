388019e1d65226e1b991571af0c75694b6b116d2
Bdiebeak <123redcreeper123@gmail.com>
2020-05-30 00:14:10 +0400

    Вращение объектов клавишами.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    /// <summary>
+    /// Скорость вращения объекта.
+    /// </summary>
+    [SerializeField]
+    private float rotationSpeed;
+
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        Quaternion newRotation = Quaternion.Euler(0f, -xInput * rotationSpeed, 0f);
+
+        transform.rotation *= newRotation;
+    }
+}
