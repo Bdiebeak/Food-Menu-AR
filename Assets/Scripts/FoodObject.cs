31f4793eaca99631d60957227ce2dae3f6ec2059
Bdiebeak <123redcreeper123@gmail.com>
2020-05-30 00:30:52 +0400

    Создал ScriptableObject для объектов еды.

---
+++ Assets/Scripts/FoodObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Food Object", menuName = "Scriptable Objects/Food Object")]
+public class FoodObject : ScriptableObject
+{
+    public Sprite photo;
+    public string name;
+    public string kkal;
+    public string fats;
+    public string carb;
+    public string protein;
+}
