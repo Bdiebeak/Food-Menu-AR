d8a845f95c205d6b84c591ad8752674faec54d23
Bdiebeak <123redcreeper123@gmail.com>
2020-05-30 00:31:36 +0400

    Настроенный прототип проекта.

--- Assets/Scripts/BurgerAnimController.cs
+++ Assets/Scripts/BurgerAnimController.cs
@@ -4,32 +4,53 @@ namespace FoodMenuAR.Assets.Scripts
 {
     public class BurgerAnimController : MonoBehaviour
     {
+        /// <summary>
+        /// Коллайдер целого блюда, который будет исчезать
+        /// при его разложении на ингредиенты.
+        /// </summary>
+        [SerializeField]
+        private GameObject burgerCollider;
+
+        /// <summary>
+        /// Animator бургера для контроля анимаций.
+        /// Инициализируется автоматически в Start().
+        /// </summary>
         private Animator anim;
+
+        /// <summary>
+        /// Текущее состояние: собран/разобран бургер.
+        /// </summary>
         private bool isAssembled = true;
 
+        /// <summary>
+        /// Инициализация переменных.
+        /// </summary>
         private void Start()
         {
             anim = GetComponent<Animator>();
         }
 
+        /// <summary>
+        /// Меняем текущее состояние бургера на противоположное.
+        /// </summary>
         public void ChangeState()
         {
             if (isAssembled)
             {
-                // Disassemble
+                // Разбираем
                 anim.SetBool("startAssembling", false);
                 anim.SetBool("startDisassembling", true);
                 isAssembled = false;
+                burgerCollider.SetActive(false);
             }
             else
             {
-                // Assemble
+                // Собираем
                 anim.SetBool("startAssembling", true);
                 anim.SetBool("startDisassembling", false);
                 isAssembled = true;
+                burgerCollider.SetActive(true);
             }
         }
-
-
     }
 }
