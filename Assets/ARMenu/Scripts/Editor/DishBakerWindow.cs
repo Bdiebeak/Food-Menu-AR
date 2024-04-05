5f482291748f4ba540199cf98d1bdf943add4b39
Bdiebeak <123redcreeper123@gmail.com>
2024-04-05 21:11:15 +0300

    Start refactoring dish baker window

--- Assets/ARMenu/Scripts/Editor/DishBakerWindow.cs
+++ Assets/ARMenu/Scripts/Editor/DishBakerWindow.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Properties;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
@@ -9,21 +10,18 @@ using Object = UnityEngine.Object;
 
 namespace ARMenu.Scripts.Editor
 {
-	// TODO: refactoring
+	// TODO: use data binding
 	public class DishBakerWindow : EditorWindow
 	{
 		public VisualTreeAsset ui;
-
-		private TextField _pathTextField;
-
-		private Camera _cameraObject;
-		private Vector2Int _resolution = new (1024, 1024);
-		private Transform _rootObject;
-		private List<Transform> _additionalObjects = new();
-		private string _savePath;
+		public Camera cameraObject;
+		public Vector2Int resolution = new (1024, 1024);
+		public Transform rootObject;
+		public List<Transform> additionalObjects = new();
+		public string savePath;
 
 		[MenuItem("Services/DishBaker")]
-		public static void ShowExample()
+		public static void ShowWindow()
 		{
 			DishBakerWindow window = GetWindow<DishBakerWindow>();
 			window.titleContent = new GUIContent(nameof(DishBakerWindow));
@@ -34,80 +32,74 @@ namespace ARMenu.Scripts.Editor
 			VisualElement root = rootVisualElement;
 			ui.CloneTree(root);
 
-			InitializeDefaultValues();
-			InitializeResolutionField(root.Q<Vector2IntField>("ResolutionField"));
-			InitializeCameraField(root.Q<ObjectField>("CameraField"));
-			InitializeRootObjectField(root.Q<ObjectField>("RootObjectField"));
-			InitializeListViewForAdditionalObjects(root.Q<ListView>("AdditionalObjectsListView"));
-			InitializePathTextField(root.Q<TextField>("FolderPathTextField"));
-			InitializeSelectPathButton(root.Q<Button>("SelectPathButton"));
+			// InitializeDefaultValues();
+			// InitializeResolutionField(root.Q<Vector2IntField>("ResolutionField"));
+			// InitializeCameraField(root.Q<ObjectField>("CameraField"));
+			// InitializeRootObjectField(root.Q<ObjectField>("RootObjectField"));
+			// InitializeListViewForAdditionalObjects(root.Q<ListView>("AdditionalObjectsListView"));
+			// InitializePathTextField(root.Q<TextField>("FolderPathTextField"));
+			// InitializeSelectPathButton(root.Q<Button>("SelectPathButton"));
 			InitializeBakeButton(root.Q<Button>("BakeButton"));
 		}
 
 		private void InitializeDefaultValues()
 		{
-			_cameraObject = Camera.main;
-			_savePath = Application.dataPath;
+			cameraObject = Camera.main;
+			savePath = Application.dataPath;
 		}
 
 		private void InitializeResolutionField(Vector2IntField resolutionField)
 		{
-			resolutionField.value = _resolution;
-			resolutionField.RegisterCallback<ChangeEvent<Vector2Int>>(evt =>
+			resolutionField.SetBinding(nameof(Vector2IntField.value), new DataBinding()
 			{
-				_resolution = evt.newValue;
+				dataSourcePath = new PropertyPath(nameof(resolution)),
+				bindingMode = BindingMode.TwoWay
 			});
 		}
 
 		private void InitializeCameraField(ObjectField cameraField)
 		{
-			cameraField.value = _cameraObject;
-			cameraField.RegisterCallback<ChangeEvent<Object>>((evt) =>
+			cameraField.SetBinding(nameof(ObjectField.value), new DataBinding()
 			{
-				_cameraObject = evt.newValue as Camera;
+				dataSourcePath = new PropertyPath(nameof(cameraObject)),
+				bindingMode = BindingMode.TwoWay
 			});
 		}
 
 		private void InitializeRootObjectField(ObjectField rootField)
 		{
-			rootField.RegisterCallback<ChangeEvent<Object>>((evt) =>
-			{
-				_rootObject = evt.newValue as Transform;
-			});
+			// rootField.RegisterCallback<ChangeEvent<Object>>((evt) =>
+			// {
+			// 	_rootObject = evt.newValue as Transform;
+			// });
 		}
 
 		private void InitializeSelectPathButton(Button pathSelectButton)
 		{
 			pathSelectButton.RegisterCallback<MouseUpEvent>(evt =>
 			{
-				_savePath = EditorUtility.OpenFolderPanel("Select Folder To Bake", Application.dataPath, "");
-				_pathTextField.value = _savePath;
+				savePath = EditorUtility.OpenFolderPanel("Select Folder To Bake", Application.dataPath, "");
 			});
 		}
 
 		private void InitializePathTextField(TextField textField)
 		{
-			_pathTextField = textField;
-			textField.value = _savePath;
-			textField.RegisterCallback<ChangeEvent<string>>((evt) =>
-			{
-				_savePath = evt.newValue;
-			});
+			textField.value = savePath;
 		}
 
 		private void InitializeListViewForAdditionalObjects(ListView listView)
 		{
-			listView.itemsSource = _additionalObjects;
+			listView.itemsSource = additionalObjects;
 			listView.makeItem = () =>
 			{
 				return new ObjectField() { objectType = typeof(Transform), allowSceneObjects = true };
 			};
 			listView.bindItem = (element, i) =>
 			{
-				((ObjectField)element).value = _additionalObjects[i];
+				((ObjectField)element).value = additionalObjects[i];
 				((ObjectField)element).RegisterValueChangedCallback((value) =>
 				{
-					_additionalObjects[i] = (Transform)value?.newValue;
+					additionalObjects[i] = (Transform)value?.newValue;
 				});
 			};
 			listView.RegisterCallback<DragUpdatedEvent>(evt =>
@@ -121,7 +113,7 @@ namespace ARMenu.Scripts.Editor
 				{
 					if (obj is GameObject gameObject)
 					{
-						_additionalObjects.Add(gameObject.transform);
+						additionalObjects.Add(gameObject.transform);
 					}
 				}
 				listView.RefreshItems();
@@ -136,29 +128,30 @@ namespace ARMenu.Scripts.Editor
 			});
 		}
 
+		// TODO: separate logic CameraBaker.Bake(root, additional, path, ...)
 		private void BakeImages()
 		{
 			// Create texture for camera baker.
-			Texture2D image = new(_resolution.x, _resolution.y);
-			RenderTexture tempRT = new(_resolution.x, _resolution.y, 24, RenderTextureFormat.ARGB32);
+			Texture2D image = new(resolution.x, resolution.y);
+			RenderTexture tempRT = new(resolution.x, resolution.y, 24, RenderTextureFormat.ARGB32);
 
 			try
 			{
 				// Bake all required object into separated files.
-				CameraBaker.SaveCameraRender(Path.Combine(_savePath, $"{_rootObject.name}.png"), _cameraObject, image, tempRT);
+				CameraBaker.SaveCameraRender(Path.Combine(savePath, $"{rootObject.name}.png"), cameraObject, image, tempRT);
 
 				// Bake additional objects
-				foreach (Transform parent in _rootObject)
+				foreach (Transform parent in rootObject)
 				{
 					parent.gameObject.SetActive(false);
 				}
-				foreach (Transform additional in _additionalObjects)
+				foreach (Transform additional in additionalObjects)
 				{
 					additional.gameObject.SetActive(true);
-					CameraBaker.SaveCameraRender(Path.Combine(_savePath, $"{additional.name}.png"), _cameraObject, image, tempRT);
+					CameraBaker.SaveCameraRender(Path.Combine(savePath, $"{additional.name}.png"), cameraObject, image, tempRT);
 					additional.gameObject.SetActive(false);
 				}
-				foreach (Transform parent in _rootObject)
+				foreach (Transform parent in rootObject)
 				{
 					parent.gameObject.SetActive(true);
 				}
