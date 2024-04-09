71f5825f66a61e3384c2676762ab2cfec7e0f616
Bdiebeak <123redcreeper123@gmail.com>
2024-04-09 17:52:45 +0300

    Set up dish description UI
    Finish UI and add simple logic

--- Assets/ARMenu/Scripts/Runtime/UI/DishDescriptionScreen.cs
+++ Assets/ARMenu/Scripts/Runtime/UI/DishDescriptionScreen.cs
@@ -5,39 +5,75 @@ namespace ARMenu.Scripts.Runtime.UI
 {
     public class DishDescriptionScreen : MonoBehaviour
     {
+        // Element keys
+        private const string ScreenRootKey = "RootVisualElement";
+        private const string ScrollViewKey = "ScrollView";
+        private const string CollapseButtonKey = "CollapseButton";
+        private const string PreviousButtonKey = "PreviousButton";
+        private const string NextButtonKey = "NextButton";
+
+        // Style keys
+        private const string MinimizedStyle = "screen-minimized";
+
         public UIDocument document;
 
-        public VisualTreeAsset minimizedDishScreen;
-        public VisualTreeAsset descriptionDishScreen;
-
-        private Button _minimizeButton;
+        // Elements
+        private VisualElement _screenRoot;
+        private ScrollView _scrollView;
+        private Button _switchCollapsedStateButton;
         private Button _previousButton;
         private Button _nextButton;
 
+        // Logic fields
+        private bool _isCollapsed;
+
         private void Awake()
         {
-            _minimizeButton = document.rootVisualElement.Q<Button>("MinimizeButton");
-            _previousButton = document.rootVisualElement.Q<Button>("PreviousButton");
-            _nextButton = document.rootVisualElement.Q<Button>("NextButton");
+            VisualElement root = document.rootVisualElement;
+
+            _screenRoot = root.Q(ScreenRootKey);
+            _scrollView = root.Q<ScrollView>(ScrollViewKey);
+            _switchCollapsedStateButton = root.Q<Button>(CollapseButtonKey);
+            _previousButton = root.Q<Button>(PreviousButtonKey);
+            _nextButton = root.Q<Button>(NextButtonKey);
+        }
+
+        private void Start()
+        {
+            SetCollapsedState(true);
         }
 
         private void OnEnable()
         {
-            _minimizeButton.RegisterCallback<ClickEvent>(OnMinimizeButtonClicked);
+            _switchCollapsedStateButton.RegisterCallback<ClickEvent>(OnMinimizeButtonClicked);
             _previousButton.RegisterCallback<ClickEvent>(OnPreviousButtonClicked);
             _nextButton.RegisterCallback<ClickEvent>(OnNextButtonClicked);
         }
 
         private void OnDisable()
         {
-            _minimizeButton.UnregisterCallback<ClickEvent>(OnMinimizeButtonClicked);
+            _switchCollapsedStateButton.UnregisterCallback<ClickEvent>(OnMinimizeButtonClicked);
             _previousButton.RegisterCallback<ClickEvent>(OnPreviousButtonClicked);
             _nextButton.RegisterCallback<ClickEvent>(OnNextButtonClicked);
         }
 
+        private void SetCollapsedState(bool state)
+        {
+            _isCollapsed = state;
+            if (state)
+            {
+                _screenRoot.AddToClassList(MinimizedStyle);
+            }
+            else
+            {
+                _screenRoot.RemoveFromClassList(MinimizedStyle);
+            }
+            _scrollView.SetEnabled(!state);
+        }
+
         private void OnMinimizeButtonClicked(ClickEvent evt)
         {
-            Debug.Log("Minimized");
+            SetCollapsedState(!_isCollapsed);
         }
 
         private void OnPreviousButtonClicked(ClickEvent evt)
